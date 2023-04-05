904c1ad7546521a59c4cde25f7c41ced638a720d
k321m <90863488+k321m@users.noreply.github.com>
2023-04-05 20:43:14 +0900

    fix: 回答フォームのをform.ejsに移植　

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
+// 被験者番号 [実行前に変更]
+const PARTICIPANT_NUM = 1;
+const fs = require("fs");
+var jsonFileName = String(PARTICIPANT_NUM).padStart(2, "0");
+
+const evaluationJsonData = JSON.parse(
+  fs.readFileSync(
+    "./json/evaluationJson/participant_" + jsonFileName + ".json",
+    "utf8"
+  )
+);
+const imageJsonData = JSON.parse(
+  fs.readFileSync(
+    "./json/imageJson/participant_" + jsonFileName + ".json",
+    "utf8"
+  )
+);
+
 var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+/* View answer form */
+router.get("/form", async function (req, res, next) {
+  let id = req.query.id;
+  let imgName = "./img/" + imageJsonData[id];
+  let evaluationKey = Object.keys(evaluationJsonData[id]);
+  res.render("form", {
+    title: "answer form",
+    img: imgName,
+    evaluation: evaluationKey,
+  });
 });
 
 module.exports = router;
