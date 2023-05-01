e0973a7a2910157f224b5020776eae43f362291b
k321m <90863488+k321m@users.noreply.github.com>
2023-05-01 14:25:38 +0900

    add: log追加

--- routes/index.js
+++ routes/index.js
@@ -114,6 +114,7 @@ router.post("/form", async function (req, res, next) {
   let id = parseInt(req.query.id) - 1;
   console.log("id:" + id);
   let participantID = req.session.participantID;
+  console.log("imageJson:" + req.session.imageJsonData);
   let imgName = req.session.imageJsonData[id];
   imgName = imgName.substring(imgName.indexOf("/") + 1, imgName.length - 4);
 
